88d6559a709dddf523b76edf62accdedbe13e806
Riya Kumari <riyakumari147369@gmail.com>
2026-01-15 12:48:41 +0530

    Prac1

---
+++ prac1.js
@@ -0,0 +1,18 @@
+//Practice Questions
+
+//1.Declare your name as a string and print its length in JavaScript.
+let name = "Riya";
+console.log(name.length);
+
+//2.Declare your first name as a string and print its first character.
+console.log(name[0]);
+
+//3.Declare your first name as a string and print its last character.
+console.log(name[name.length-1]);
+
+//4.What is the output of the following code: "apnacollege" + 123
+console.log("apnacollege" + 123);
+
+//5.What are the lengths of: an empty string & a string with a single space?
+console.log("".length);
+console.log(" ".length);
